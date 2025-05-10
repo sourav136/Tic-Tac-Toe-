6b43fc8f9a8ef9b2addf48d889e2274f8d132f4a
Sourav <souravbarua789bd@gmail.com>
2025-05-10 23:42:56 +0600

    added logics to show cross and circle

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Home.css"
 import Retry from '../images/Vector.png'
 
 const Home = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [isXTurn, setIsXTurn] = useState(true)
+
+    const hendleClick = (index) =>{
+        if (board[index]) return;
+
+        const newBoard = [...board];
+        newBoard[index] = isXTurn ? 'X' : 'O'
+        setBoard(newBoard);
+        setIsXTurn(!isXTurn)
+        console.log("clicked", index)
+    }
+
     return (
         <>
             <div className="home">
                 <h1 className='notification'>Your Turn</h1>
                 <p className='game-status'>You Win!</p>
                 <div className="box-container">
-                    <div className="box cross"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box circle"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                    {
+                        board.map((value, index) => (
+                            <div className="box" key={index} onClick={() => hendleClick(index)}>
+                                {value && <span className={value === "X" ? "cross" : "circle"}></span>}
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <button><img src={Retry} alt="Retry icon" className='retry' />Play Again</button>
