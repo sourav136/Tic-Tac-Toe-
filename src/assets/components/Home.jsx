62ec4d8f6ea36333ca7d3f60a9fe246f01069c22
Sourav <souravbarua789bd@gmail.com>
2025-05-17 09:59:06 +0600

    added logics to determine winner or draw

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -5,22 +5,64 @@ import Retry from '../images/Vector.png'
 const Home = () => {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [isXTurn, setIsXTurn] = useState(true)
+    const [winner, setWinner] = useState(null)
+
+    const winningPattern = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+    ]
+
+    const checkWinner = (board) => {
+        for (let combo of winningPattern){
+            const [a,b,c] = combo
+            if (board[a] && board[a]===board[b] && board[a]===board[c]) {
+                return board[a]
+            }
+        }
+        return null;
+    }
 
     const hendleClick = (index) =>{
-        if (board[index]) return;
+        if (board[index] || winner) return;
 
         const newBoard = [...board];
         newBoard[index] = isXTurn ? 'X' : 'O'
         setBoard(newBoard);
         setIsXTurn(!isXTurn)
         console.log("clicked", index)
+
+        const gameWinner = checkWinner(newBoard)
+        if (gameWinner){
+            setWinner(gameWinner)
+        }
+
+        if (!newBoard.includes(null) && !gameWinner){
+            setWinner("Draw")
+        }
     }
 
     return (
         <>
             <div className="home">
-                <h1 className='notification'>Your Turn</h1>
-                <p className='game-status'>You Win!</p>
+                <h1 className='notification'>
+                    {
+                        winner? (winner === "Draw" ? "Oops!!" : "Congratulations!!") : `${isXTurn ? "X" : "O"}'s turn`
+                    }
+                </h1>
+                {
+                    winner && (
+                        <p className='game-status'>
+                            {winner === "Draw" ? "It's a draw.. Try again!" : `${winner} Wins!!!`}
+                        </p>
+
+                    )
+                }
                 <div className="box-container">
                     {
                         board.map((value, index) => (
