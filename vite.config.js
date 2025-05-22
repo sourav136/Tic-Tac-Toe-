bf1cfa78e1b97cf591b527eb541df173d0904b8d
Sourav <souravbarua789bd@gmail.com>
2025-05-22 23:52:37 +0600

    made the project available offline

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { makeOffline } from "vite-plugin-make-offline";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), makeOffline()], 
 })
