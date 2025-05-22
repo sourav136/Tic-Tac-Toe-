42336f24cfd267f7e5e10d0831360c8777990638
Sourav <souravbarua789bd@gmail.com>
2025-05-22 23:59:26 +0600

    replaced BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter , Routes, Route } from 'react-router-dom';
 import Mode from './components/Mode';
 import HumanVSHuman from './pages/HumanVSHuman';
 import HumanVSBot from './pages/HumanVSBot';
@@ -10,13 +10,13 @@ const App = () => {
   return (
     <>
       <GameProvider>
-        <BrowserRouter>
+        <HashRouter >
           <Routes>
             <Route exact path='/' Component={Mode}/>
             <Route exact path='/human' Component={HumanVSHuman}/>
             <Route exact path='/bot' Component={HumanVSBot}/>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </GameProvider>
     </>
   );
